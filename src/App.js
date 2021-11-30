6e2592c0aaf97ed1130ed8ee311f6e653e3c427d
adkhan94 <adnan.pisces3@gmail.com>
2021-11-30 23:03:08 +0500

    Add pages in app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import Header from './Components/Header';
+ import NotesPage from './Pages/NotesPage.js'; 
+
+function App() {
+  return (
+    <div>
+       <NotesPage />
+    </div>
+  );
+}
+
+export default App;
