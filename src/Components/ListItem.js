f415288dbb515314606e641fdae59828c92ffad0
adkhan94 <adnan.pisces3@gmail.com>
2021-12-11 15:54:21 +0500

    Add styling and routes to the app

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const ListItem = ({note}) => {
     console.log('>>>', note.body)
     return (
-        <div className="notes-list-item">
-            <h3>
-                {note.body}
-            </h3>
 
-            
-        </div>
+        <Link to={`/note/${note.id}`}>
+            <div className="notes-list-item">
+                <h3>
+                    {note.body}
+                </h3>
+            </div>
+        </Link>
     )
 }
 
--- src/Pages/NotesPage.js
+++ src/Pages/NotesPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import notes from '../assets/data';
-import ListItem from '../Components/ListItem'
+import ListItem from '../Components/ListItem';
+import {Link} from 'react-router-dom';
 
 
 const NotesPage = () => {
     return (
-        <div>
-            {notes.map(note => 
-                <ListItem note={note}  /> 
-                )}
+        <div className="notes">
+            <div className="note-header">
+                <h2 className="notes-title"> &#9782; Notes </h2>
+                <p className="notes-count">{notes.length} </p>
+            </div>
+            <div className="notes-list">
+                {notes.map((note, index) =>  
+                    <ListItem note={note} key={index}   /> 
+                    )}
+            </div>
         </div>
     )
 }
